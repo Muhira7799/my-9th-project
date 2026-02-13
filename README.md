481b5ab6b75814f8b1fa342ec5f6806e806d24ed
Muhira7799 <muhiramohammed99@gmail.com>
2026-02-13 01:24:39 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my-9th-project
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Muhira7799/my-9th-project)
---
+++ index.ts
@@ -0,0 +1,13 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let number = prompt("enter a number:");
+number = Number(number);
+if (number>10){
+    console.log("the number is greater than 10.");
+}else if (number<10){
+    console.log("the number is less than 10.");
+}else{
+    console.log("the number is exactly 10.");
+}
